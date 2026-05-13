9caafea96cb749782ee11e15750c60423463efe0
Snt <sant85082@gmail.com>
2026-05-12 23:13:01 -0500

    feat: comenzar la implementacion del firmware para la pulsera

--- firmware/pulsera/lib/comunicacion/semaforo/comunicacion_semaforo.h
+++ firmware/pulsera/lib/comunicacion/semaforo/comunicacion_semaforo.h
@@ -9,11 +9,20 @@
 #include <WiFi.h>
 
 inline void iniciarDispositivo() {
+    //Establecemos la frecuencia del dispositivo
+    Serial.begin(115200);
+    //Encendemos el chip de WiFi
     WiFi.mode(WIFI_STA);
 
     if (esp_now_init() != ESP_OK) {
-        // TODO: manejar el error de inicialización.
+        Serial.println("Error en la inicializacion de ESP-Now");
+        return;
     }
 }
+
+void OnDataRecv(const uint8_t *mac, const uint8_t *incomingData, int len) {
+
+}
+
 
 #endif
---
+++ firmware/pulsera/lib/comunicacion/semaforo/estructura_semaforo.h
@@ -0,0 +1,10 @@
+#ifndef PULSERA_ESTRUCTURA_SEMAFORO_H
+#define PULSERA_ESTRUCTURA_SEMAFORO_H
+
+typedef struct estado_semaforo {
+    bool permitidoCruce;
+};
+
+estado_semaforo estado;
+
+#endif //PULSERA_ESTRUCTURA_SEMAFORO_H
