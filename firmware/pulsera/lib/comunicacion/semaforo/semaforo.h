8025ef391ee1ea48d515d5b836823b35f35c3e71
Snt <sant85082@gmail.com>
2026-05-17 14:21:32 -0500

    feat: implementar la estructura para la comunicacion de la pulsera mediante ESP-NOW

--- firmware/pulsera/lib/comunicacion/semaforo/semaforo.h
+++ firmware/pulsera/lib/comunicacion/semaforo/semaforo.h
@@ -1,28 +1,22 @@
 #ifndef COMUNICACION_SEMAFORO_H
 #define COMUNICACION_SEMAFORO_H
 
-//Incluimos la libreria de Arduino
 #include <Arduino.h>
-
-//Incluimos las librerias para esp_now
-#include <esp_now.h>
+#include <stdint.h> //Incluimos para usar enteros de un byte
 #include <WiFi.h>
+#include <esp_now.h>
+#include "../protocolo/protocolo_esp.h" //Incluimos para usar la estructura de comunicacion
 
-inline void iniciarDispositivo() {
-    //Establecemos la frecuencia del dispositivo
-    Serial.begin(115200);
-    //Encendemos el chip de WiFi
-    WiFi.mode(WIFI_STA);
+class ComunicadorSemaforo {
+private:
+    static const uint8_t BROADCAST_MAC[6]; //Direccion MAC
+public:
+    static estado_semaforo ultimoEstado; //Ultimo paquete de datos recibido
+    static bool inicializar(); //Establece la comunicacion con el Semaforo
+    static bool desinicializar(); //Detiene la comunicacoin con el Semaforo
+private:
+    static void onDatosRecibidos(const uint8_t *mac_addr, const uint8_t *data, int data_len); //
 
-    if (esp_now_init() != ESP_OK) {
-        Serial.println("Error en la inicializacion de ESP-Now");
-        return;
-    }
-}
-
-void OnDataRecv(const uint8_t *mac, const uint8_t *incomingData, int len) {
-
-}
-
+};
 
 #endif
