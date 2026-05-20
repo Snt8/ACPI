ee9fad3ba1eabce005eecab63d00cd67e2f492e7
Snt <sant85082@gmail.com>
2026-05-19 22:57:19 -0500

    feat:implementar la estructura de cabecera de control de los motores

---
+++ firmware/pulsera/lib/motores/motor.h
@@ -0,0 +1,27 @@
+#ifndef MOTOR_H
+#define MOTOR_H
+#include <stdint.h>
+
+//Definimos la clase que controlara los motores
+class ControladorMotores {
+private:
+    //Motor derecho
+    static const uint8_t pin_motor_derecho = 5;
+    static const uint8_t canal_motor_derecho = 0;
+    //Motor izquierdo
+    static const uint8_t pin_motor_izquierdo = 4;
+    static const uint8_t canal_motor_izquierdo = 1;
+    //Configuraciones
+    static const uint16_t frecuencia = 1000;
+    static const uint8_t resolucion = 8;
+public:
+    enum Motor { DERECHO, IZQUIERDO, AMBOS};
+
+    //Inicializa ambos motores
+    static bool inicializar();
+    //Hace vibrar el motor pasado como argumento
+    static void vibrarMotor(Motor motor, uint8_t intensidad);
+    //Detiene el motor pasado como argumento
+    static void detenerMotor(Motor motor);
+};
+#endif
