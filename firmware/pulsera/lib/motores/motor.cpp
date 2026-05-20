b7b8ee73db7a41ae998c3ffb531b96b0b1a08ae4
Snt <sant85082@gmail.com>
2026-05-19 22:57:44 -0500

    feat: implementar logica del control de los motores

--- firmware/pulsera/lib/motores/motor.cpp
+++ firmware/pulsera/lib/motores/motor.cpp
@@ -1,1 +1,67 @@
-#include
+//Incluimos la cabecera
+#include "motor.h"
+//Incluimos otras bibliotecas necesarias
+#include <stdint.h>
+#include <Arduino.h>
+//Definimos constantes globales
+#define APAGAR_MOTOR 0
+
+bool ControladorMotores::inicializar() {
+    //Ejecutamos ledcSetup para inicializar el motor derecho, si falla; retornamos
+    if (ledcSetup(canal_motor_derecho, frecuencia, resolucion) == 0) {
+        return false;
+    }
+    //Conectamos el pin del motor derecho
+    ledcAttachPin(pin_motor_derecho, canal_motor_derecho);
+    //Ejecutamos ledcSetup par inicializar ahora el motor izquierdo
+    if (ledcSetup(canal_motor_izquierdo, frecuencia, resolucion) == 0) {
+        return false;
+    }
+    //Conectamos el pin del motor izquierdo
+    ledcAttachPin(pin_motor_izquierdo, canal_motor_izquierdo);
+    //Ejecutamos ledcSetup para inicializar el motor izquierdo, si falla; retornamos
+    return true;
+}
+
+void ControladorMotores::vibrarMotor(Motor motor, uint8_t intensidad) {
+    //Evaluamos el caso del motor que queremos activar
+    switch(motor) {
+        case DERECHO:
+            //Encendemos el motor derecho
+            ledcWrite(canal_motor_derecho, intensidad);
+            break;
+
+        case IZQUIERDO:
+            //Encendemos el motor izquierdo
+            ledcWrite(canal_motor_izquierdo, intensidad);
+            break;
+
+        case AMBOS:
+            //Encendemos ambos motores
+            ledcWrite(canal_motor_derecho, intensidad);
+            ledcWrite(canal_motor_izquierdo, intensidad);
+            break;
+    }
+}
+
+void ControladorMotores::detenerMotor(Motor motor) {
+    //Evaluamos los casos de los motores para apagarlos
+    switch(motor) {
+        case DERECHO:
+            //Apagamos el motor derecho
+            ledcWrite(canal_motor_derecho, APAGAR_MOTOR);
+            break;
+
+        case IZQUIERDO:
+            //Apagamos el motor izquierdo
+            ledcWrite(canal_motor_izquierdo, APAGAR_MOTOR);
+            break;
+
+        case AMBOS:
+            //Apagamos ambos
+            ledcWrite(canal_motor_derecho, APAGAR_MOTOR);
+            ledcWrite(canal_motor_izquierdo, APAGAR_MOTOR);
+            break;
+    }
+}
+
