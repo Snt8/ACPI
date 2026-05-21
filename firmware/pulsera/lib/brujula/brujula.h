a04d00c890d93a0d45df9720dd6d0ebf5f507e7a
Snt <sant85082@gmail.com>
2026-05-21 00:02:16 -0500

    refactor: exponer atributo del heading actual de la brujula

--- firmware/pulsera/lib/brujula/brujula.h
+++ firmware/pulsera/lib/brujula/brujula.h
@@ -5,9 +5,8 @@
 
 //Definimos la clase que gestionara la brujula (magnetometro y acelerometro)
 class ControladorBrujula {
-private:
-    static float headingActual;
 public:
+    static float headingActual;
     static bool inicializar(); //Inicializa ambos sensores
     static void obtenerHeading(); //Lee los sensores y retorna el heading compensado
     static bool revisarSemaforo(uint16_t posicionSemaforo); //Compara en base al heading para determinar si es el semaforo correcto
---
+++ firmware/pulsera/lib/utils/indicar_error.h
@@ -0,0 +1,8 @@
+//
+// Created by sntav on 20/05/2026.
+//
+
+#ifndef PULSERA_INDICAR_ERROR_H
+#define PULSERA_INDICAR_ERROR_H
+
+#endif //PULSERA_INDICAR_ERROR_H
