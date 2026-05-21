52657225ec1454b59594cf843d11244e4d8334b0
Snt <sant85082@gmail.com>
2026-05-20 22:52:57 -0500

    feat: implementar la logica de la funcion para determinar la direccion del semaforo

---
+++ firmware/pulsera/lib/utils/calcular_direccion.cpp
@@ -0,0 +1,14 @@
+//Incluimos la cabecera
+#include "calcular_direccion.h"
+//Incluimos bibliotecas necesarias
+#include <Arduino.h>
+//Definimos constantes globales
+#define GRADOS_CIRCUNFERENCIA 360.0
+
+float calcularDireccion(float posicionSemaforo, float headingActual) {
+    float diferencia = fmod(posicionSemaforo - headingActual + GRADOS_CIRCUNFERENCIA, GRADOS_CIRCUNFERENCIA);
+    if (diferencia > GRADOS_CIRCUNFERENCIA / 2) {
+        diferencia -= GRADOS_CIRCUNFERENCIA;
+    }
+    return diferencia;
+}
