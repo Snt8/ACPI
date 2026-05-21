5e92693d9a0dc3a578ba5976bc3bd8bec029fab2
Snt <sant85082@gmail.com>
2026-05-20 22:28:00 -0500

    feat: implementar logica de la funcion para controlar los momentos de vibracion de los motores

--- firmware/pulsera/lib/utils/patrones_vibracion.cpp
+++ firmware/pulsera/lib/utils/patrones_vibracion.cpp
@@ -1,3 +1,22 @@
-//
-// Created by sntav on 20/05/2026.
-//
+//Incluimos la libreria de arduino
+#include <Arduino.h>
+//Inclumos el archivo de cabecera
+#include "patrones_vibracion.h"
+//Incluimos motores/motor.h para utilizar la coleccion Motor
+#include "../motores/motor.h"
+
+void controlarVibracion(unsigned long milisegundos, unsigned long &ultimo_tiempo_vibracion, bool &motor_encendido, ControladorMotores::Motor motor, uint16_t intensidad) {
+    unsigned long tiempo_actual = millis();
+    //Evaluamos si el tiempo transcurrido desde la ultima vez es el estipulado
+    if (((tiempo_actual - ultimo_tiempo_vibracion) >= milisegundos) && !motor_encendido) {
+        ControladorMotores::vibrarMotor(motor, intensidad);
+        motor_encendido = true;
+        ultimo_tiempo_vibracion = tiempo_actual;
+    }
+
+    if (((tiempo_actual - ultimo_tiempo_vibracion >= milisegundos)) && motor_encendido) {
+        ControladorMotores::detenerMotor(motor);
+        motor_encendido = false;
+        ultimo_tiempo_vibracion = tiempo_actual;
+    }
+}
