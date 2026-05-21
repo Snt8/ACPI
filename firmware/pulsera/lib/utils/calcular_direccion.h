25b9c1f73d36b232f2db429313390e828c8f6269
Snt <sant85082@gmail.com>
2026-05-20 22:52:23 -0500

    feat: implementar la cabecera de la funcion para determinar la direccion del semaforo

---
+++ firmware/pulsera/lib/utils/calcular_direccion.h
@@ -0,0 +1,7 @@
+#ifndef PULSERA_CALCULAR_DIRECCION_H
+#define PULSERA_CALCULAR_DIRECCION_H
+
+//Definimos la funcion para determinar la orientacion del semaforo
+float calcularDireccion(float posicionSemaforo, float headingActual);
+
+#endif
