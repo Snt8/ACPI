10d9398f9c3ef1e3d38c8df28bf1aca5e341d7b0
Snt <sant85082@gmail.com>
2026-05-21 00:03:47 -0500

    feat: implementar la cabecera de la vibracion para indicar un error en la inicializacion de la pulsera

--- firmware/pulsera/lib/utils/indicar_error.h
+++ firmware/pulsera/lib/utils/indicar_error.h
@@ -1,8 +1,7 @@
-//
-// Created by sntav on 20/05/2026.
-//
-
 #ifndef PULSERA_INDICAR_ERROR_H
 #define PULSERA_INDICAR_ERROR_H
 
-#endif //PULSERA_INDICAR_ERROR_H
+//Definimos la funcion que dara los pulsos de los motores si ocurre un error
+void indicarError();
+
+#endif
