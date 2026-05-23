66ad9bfe00d83e16106c875648d166f2dd5bf074
Snt <sant85082@gmail.com>
2026-05-23 02:14:58 -0500

    feat: implementar parametros y constantes de configuracion

---
+++ firmware/pulsera/include/config.h
@@ -0,0 +1,13 @@
+#ifndef CONFIG_H
+#define CONFIG_H
+
+//Definimos los valores de configuracion
+
+//Direccion y semaforo
+#define UMBRAL_SEMAFORO 45.0
+//Establecer frecuencias
+#define BAUD_RATE 115200
+//Establecer duracion entre vibraciones
+#define INTERVALOS_MILISEGUNDOS 500
+
+#endif
---
+++ firmware/pulsera/include/constantes.h
@@ -0,0 +1,16 @@
+#ifndef CONSTANTES_H
+#define CONSTANTES_H
+
+//Definimos las constantes globales
+
+//Circunferencia
+#define GRADOS_CIRCUNFERENCIA 360.0
+#define RAD_A_GRADOS (180.0 / M_PI)
+#define GRADOS_A_RAD (M_PI / 180.0)
+//Configuracion de los motores
+#define APAGAR_MOTOR 0
+#define INTENSIDAD_MOTOR 255
+//Conversion de datos
+#define FACTOR_ESCALA 16384.0
+
+#endif
